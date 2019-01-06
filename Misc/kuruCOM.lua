f6d2f5f1e8af4b9986c0cdb1df4b0cd969456eba
E-Sh4rk <esh4rk@gmail.com>
2019-01-06 12:36:19 +0100

    update ram watch & kuruCOM

--- Misc/kuruCOM.lua
+++ Misc/kuruCOM.lua
@@ -6,6 +6,17 @@ local out_filename = "out.txt"
 
 -----------------------------
 
+local addr_x_pos      = 0x4544
+local addr_y_pos      = 0x4548
+local addr_xb     = 0x454C
+local addr_yb     = 0x4550
+local addr_xs     = 0x4554
+local addr_ys     = 0x4558
+local addr_rotation     = 0x4572
+local addr_rotation_rate   = 0x4574
+local addr_rotation_srate   = 0x4576
+
+
 local in_file = working_dir .. "/" .. in_filename
 local out_file = working_dir .. "/" .. out_filename
 
@@ -43,7 +54,7 @@ function modify(filename, modify_func)
   write(filename, contents)
 end
 -- Clean string
-function clean_str(str)
+function normalize_str(str)
 	if (str == nil) then return "" end
   local res = bizstring.replace(str, "\r", "")
   return bizstring.trim(res)
@@ -58,7 +69,8 @@ do
 		local content = lines_from(in_file)
 		--for i=1, #content do end
 		if #content > 0 then
-			if clean_str(content[1]) == "DUMPMAP" then
+			local cmd = normalize_str(content[1])
+			if cmd == "DUMPMAP" then
 				local xl = memory.read_u16_le(0x0, "EWRAM")
 				local yl = memory.read_u16_le(0x2, "EWRAM")
 				local res = tostring(xl) .. " " .. tostring(yl) .. "\n"
@@ -71,6 +83,36 @@ do
 				end
 				write_file(out_file, res)
 				os.remove(in_file)
+				
+			elseif cmd == "GETPOS" then
+				local xpos = memory.read_u32_le(addr_x_pos, "IWRAM")
+				local ypos = memory.read_u32_le(addr_y_pos, "IWRAM")
+				local rot = memory.read_u16_le(addr_rotation, "IWRAM")
+				local srot = memory.read_s16_le(addr_rotation_srate, "IWRAM")
+				local res = tostring(xpos) .. " " .. tostring(ypos) .. "\n" .. tostring(rot) .. " " .. tostring(srot) .. "\n"
+				write_file(out_file, res)
+				os.remove(in_file)
+				
+			elseif cmd == "PLAY" then
+				if #content >= 3 then
+					-- Init
+					local pos = bizstring.split(normalize_str(content[2]), " ")
+					local rot = bizstring.split(normalize_str(content[3]), " ")
+					local xpos = tonumber(pos[1]) % 0x100000000
+					local ypos = tonumber(pos[2]) % 0x100000000
+					local rot = tonumber(rot[1]) % 0x10000
+					local srot = tonumber(rot[2])
+					memory.write_u32_le(addr_x_pos, xpos, "IWRAM")
+					memory.write_u32_le(addr_y_pos, ypos, "IWRAM")
+					memory.write_u32_le(addr_xb, 0, "IWRAM")
+					memory.write_u32_le(addr_yb, 0, "IWRAM")
+					memory.write_u32_le(addr_xs, 0, "IWRAM")
+					memory.write_u32_le(addr_ys, 0, "IWRAM")
+					memory.write_u16_le(addr_rotation, rot, "IWRAM")
+					memory.write_s16_le(addr_rotation_rate, srot, "IWRAM")
+					memory.write_s16_le(addr_rotation_srate, srot, "IWRAM")
+					-- Play inputs
+				end
 			end
 		end
 	end
