bc32fdef5668d8285615fd2f1e88c47064f502bb
E-Sh4rk <esh4rk@gmail.com>
2019-01-14 20:59:48 +0100

    spring analysis

--- Misc/Kururin_OoB_ending_finder.lua
+++ Misc/Kururin_OoB_ending_finder.lua
@@ -6,21 +6,30 @@ local addr_map_x_size = 0x313C
 local addr_map_y_size = 0x313E
 local addr_x_pos      = 0x4546
 local addr_y_pos      = 0x454A
+local addr_rotation_srate   = 0x4576
 local tile_size       = 8
 
 local map_x_size = memory.read_u16_le(addr_map_x_size, "IWRAM")
 local map_y_size = memory.read_u16_le(addr_map_y_size, "IWRAM")
+local rot_srate = memory.read_s16_le(addr_rotation_srate, "IWRAM")
 
 local init_x_pos = memory.read_u16_le(addr_x_pos, "IWRAM")
 local init_y_pos = memory.read_u16_le(addr_y_pos, "IWRAM")
 
-for y=-map_y_size, 0 do
-	for x=0, map_x_size do
+for y=0,map_y_size do -- y=-map_y_size, 0 do
+	for x=-map_x_size, 0 do -- x=0, map_x_size do
 		-- Go to the sprite position
 		x_pos = x*tile_size + tile_size/2
 		y_pos = y*tile_size + tile_size/2
 		memory.write_s16_le(addr_x_pos, x_pos, "IWRAM")
 		memory.write_s16_le(addr_y_pos, y_pos, "IWRAM")
+		-- Print location of springs and ending zones
+		local new_rot_srate = memory.read_s16_le(addr_rotation_srate, "IWRAM")
+		if new_rot_srate ~= rot_srate
+		then
+			console.log(tostring(x_pos) .. " " .. tostring(y_pos))
+			rot_srate = new_rot_srate
+		end
 		emu.frameadvance()
 		-- Go back to initial position to heal
 		memory.write_u16_le(addr_x_pos, init_x_pos, "IWRAM")
